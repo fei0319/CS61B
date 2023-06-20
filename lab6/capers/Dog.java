546649c9f806329e740ccfbc2cc343b0019ff240
Fei Pan <pan-fei@outlook.com>
2023-06-20 15:47:39 +0800

    Finished lab6/dog

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -5,13 +5,12 @@ import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Fei Pan
 */
-public class Dog { // TODO
+public class Dog implements Serializable {
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = CapersRepository.DOGS_FOLDER;
 
     /** Age of dog. */
     private int age;
@@ -39,8 +38,7 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        return Utils.readObject(new File(name), Dog.class);
     }
 
     /**
@@ -56,7 +54,7 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        Utils.writeObject(new File(name), this);
     }
 
     @Override
