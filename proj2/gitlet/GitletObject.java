397e70dc2b967471b250adc75eaf1a58c064f9e6
Fei Pan <pan-fei@outlook.com>
2023-06-21 15:14:18 +0800

    Added GitletObject

--- proj2/gitlet/GitletObject.java
+++ proj2/gitlet/GitletObject.java
@@ -3,12 +3,32 @@ package gitlet;
 import java.io.Serializable;
 import java.io.File;
 
+/**
+ * The GitletObject class is intended to mimic the four types of objects in real git.
+ * It guarantees that its subclasses are serializable and can be conveniently stored.
+ * @author Fei Pan
+ */
 public interface GitletObject extends Serializable {
+    /**
+     * Get SHA1 value of the object.
+     * @return SHA1 value of the object
+     */
     String sha1();
-    default void store(File gitletDir) {
+
+    /**
+     * Store the object to a path determined by its SHA1 value,
+     * namely {@code <gitletDir>/objects/<SHA1[:2]>/<SHA1[2:]>}.<br>
+     * Will return SHA1 value of the object.
+     * @param gitletDir the {@code .gitlet} directory
+     * @return SHA1 value of the object
+     */
+    default String store(File gitletDir) {
         String fullName = sha1();
         String prefix = fullName.substring(0, 2), suffix = fullName.substring(2);
-        File target = Utils.join(gitletDir, prefix, suffix);
-        Utils.writeContents(target, this);
+        File targetDir = Utils.join(gitletDir, "objects", prefix);
+        File target = Utils.join(targetDir, suffix);
+        targetDir.mkdirs();
+        Utils.writeObject(target, this);
+        return fullName;
     }
 }
