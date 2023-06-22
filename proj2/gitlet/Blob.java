7832567f625a5c59e900b7fc71667f39e432b0a8
Fei Pan <pan-fei@outlook.com>
2023-06-22 11:41:18 +0800

    Finished blob

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -1,13 +1,28 @@
 package gitlet;
 
+import java.io.File;
+
+/**
+ * Blob represents binary large object,
+ * and can be used to store content of a file.
+ * @author Fei Pan
+ */
 public class Blob implements GitletObject {
     private byte[] data;
     /**
      * Creates a Blob object from the specified file.
      * @param f file to create Blob from
      */
-    public Blob(java.io.File f) {
+    public Blob(File f) {
         this.data = Utils.readContents(f);
+    }
+
+    /**
+     * Save the blob to a file.
+     * @param f file to save the blob to
+     */
+    public void saveAs(File f) {
+        Utils.writeContents(f, (Object) data);
     }
     public String sha1() {
         return Utils.sha1((Object) data);
---
+++ proj2/gitlet/BlobTest.java
@@ -0,0 +1,23 @@
+package gitlet;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.File;
+
+/**
+ * Used to test functionality of Blob.
+ * @author Fei Pan
+ */
+public class BlobTest {
+    @Test
+    public void testReadWrite() {
+        final String FILE_NAME = ".blobtest";
+        File f = new File(FILE_NAME), g = new File(FILE_NAME + "1");
+        Utils.writeContents(f, "Hello!");
+
+        Blob b = new Blob(f);
+        b.saveAs(g);
+        Blob c = new Blob(g);
+        Assert.assertEquals(b.sha1(), c.sha1());
+    }
+}
